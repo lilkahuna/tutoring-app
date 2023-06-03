4546d7cc2dae60db9c7bcc7d37c6df9d44cc2edb
lilkahuna <brodie.liverman@gmail.com>
2023-06-03 12:27:19 -0400

    More UI changes/gradient changes as well.

--- src/App.js
+++ src/App.js
@@ -20,15 +20,17 @@ function App() {
         </p>
       </div>
       
-      <div className='bg-sky-300 rounded-2xl shadow-lg justify-evenly flex flex-row mx-6 p-5'> 
+      <div className='bg-gradient-to-t from-yellow-200 via-30% to-sky-300 
+        rounded-2xl shadow-lg justify-evenly flex flex-row mx-6 p-5'> 
           
-          <div className='border-s-slate-50 p-2 mx-3 w-1/4 text-center border-solid border-2 border-white rounded-xl'>
+          <div className='border-s-slate-50 p-2 mx-3 w-1/4 text-center border-double border-2 border-white rounded-xl'>
             
             <h1 className='text-xl'>Get an Appointment Now</h1>  
 
             <p className='my-4 text-md text-sky-950'>Book a tutoring appointment today and gain confidence in 
               subjects like math, science, and English with the guidance of an experienced and dedicated tutor.</p>
-
+            
+            <button className=''>Schedual Now</button>
           </div>
           
           <div className='border-s-slate-50 mx-3 '>

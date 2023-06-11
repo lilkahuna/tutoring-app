bf6c8df4e2ce10661cd6ddc714ba09fbf6d61201
lilkahuna <brodie.liverman@gmail.com>
2023-06-10 22:23:13 -0400

    migrating over to typescript.

--- src/Components/Wave.js
+++
@@ -1,9 +0,0 @@
-export const Wave = () => {
-  
-    return (
-    <div class="ocean">
-        <div class="wave"></div>
-        <div class="wave"></div>
-    </div>
-  )
-}
---
+++ src/Components/Wave.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Wave = () => {
+  
+    return (
+    <div className="ocean">
+        <div className="wave"></div>
+        <div className="wave"></div>
+    </div>
+  )
+}

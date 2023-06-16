f92e9aa32dd80ea8933d3491fe1e475729e9874c
lilkahuna <90534314+lilkahuna@users.noreply.github.com>
2023-06-16 00:33:09 -0400

    Added button component.

---
+++ src/Components/ButtonLink.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+interface Props{
+    text: string
+    path: string
+    icon?:  React.JSX.Element
+}
+
+const ButtonLink: React.FC<Props> = ({ text, icon, path }) => {
+  return (
+    <Link  
+    className='bg-transparent 
+    hover:bg-blue-500 text-blue-700 font-semibold 
+    hover:text-white py-2 px-4 
+    border border-blue-500 hover:border-transparent rounded 
+    fixed float-left bottom-1/3 mx-10' to={path}>{text} {icon}</Link>
+  )
+}
+
+export default ButtonLink;
--- src/Pages/Qualifications.tsx
+++ src/Pages/Qualifications.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import ButtonLink from '../Components/ButtonLink'
+import { FaBeer } from 'react-icons/fa'
 
 const Qualifications = () => {
   return (
-    <div>Qualifications</div>
+    <>
+      <div>Qualifications</div>
+      <ButtonLink text='Back' icon={<FaBeer />} path='/'/>
+    </>
   )
 }
 

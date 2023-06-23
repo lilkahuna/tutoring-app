8f92ac4d948b76c129ce9bd56410fd1190e098c9
lilkahuna <brodie.liverman@gmail.com>
2023-06-23 16:20:27 -0400

    Still get an error.

--- src/Pages/Schedule.tsx
+++ src/Pages/Schedule.tsx
@@ -1,30 +1,36 @@
-import React, { MutableRefObject, RefObject, useRef } from 'react'
+import React, { useRef } from 'react'
 import ButtonLink from '../Components/ButtonLink'
 import { TbArrowBackUp, TbArrowForwardUp } from 'react-icons/tb'
 import Form from '../Components/Form'
 import emailjs from '@emailjs/browser'
 
-const Schedule = () => {
-  const formRef = useRef()
+const Schedual = () => {
 
-
+  const formRef = useRef<HTMLFormElement>(null)
   const handleSchedule = (e: React.FormEvent) => {
     e.preventDefault()
-    
+
+    if (formRef.current) {
+      console.log(formRef.current)
+    }
+    else{
+      console.log(formRef.current)
+    }
+
+  }
+  /** 
     emailjs.sendForm('service_os6o6t6', 'template_ajedy5a', formRef.current, 'YlBlgsE3qya_lU-_j')
-      .then((result) => {
-        console.log(result.text);
-      }, (error) => {
-        console.log(error.text);
-      });
-    
-  };
-  
+    .then((result) => {
+      console.log(result.text);
+    }, (error) => {
+      console.log(error.text);
+    });
+    */
   return (
 
     <>
       <div>
-        <Form title="Schedule Here" onSubmit={handleSchedule} isSelectVisible={true} ref={formRef}/>
+        <Form title="Schedule Here" onSubmit={handleSchedule} isSelectVisible={true} ref={formRef} />
       </div>
       <ButtonLink text="Back" path="/" icon={<TbArrowBackUp />} />
       <ButtonLink text="Issues" path="/issues" icon={<TbArrowForwardUp />} />
@@ -33,4 +39,4 @@ const Schedule = () => {
 }
 
 
-export default Schedule 
+export default Schedual
